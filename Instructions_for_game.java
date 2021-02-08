bf94872f8059dbde62b01c5495533c07587a534b
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-02-08 17:16:07 +0530

    Instructions for number of players

---
+++ Instructions_for_game.java
@@ -0,0 +1,10 @@
+package Snake_and_Ladder;
+
+public class Instructions_for_game {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Snake and Ladder game played with single player at start position 0");
+	}
+
+}
